458f3481051e5556fd09be0d3dfad7592985edaa
AbrahamHub <apple_abraham@hotmail.com>
2022-09-26 20:29:09 -0500

    1

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+      return (<>
+        <img src='' alt="Imagen de tecnologías" srcset="" />
+        <p>hola</p>
+     </>);
+    }
+  }
+  
+  export default Header;
